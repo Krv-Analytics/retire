dc9fbd89dd9c63d1566cca9007968e0410d4c2aa
Stuart Wayland <swayland@ucsc.edu>
2025-07-01 21:06:21 -0700

    adding sync data script to pull from google sheet

---
+++ scripts/script_utils.py
@@ -0,0 +1,13 @@
+import subprocess
+from pathlib import Path
+
+
+def get_repo_root() -> Path:
+    return Path(
+        subprocess.run(
+            ["git", "rev-parse", "--show-toplevel"],
+            capture_output=True,
+            text=True,
+            check=True,
+        ).stdout.strip()
+    )
