377cb9cea1c2786448ef14a72c54369e1787bdb4
Stuart Wayland <swayland@ucsc.edu>
2025-07-11 16:13:02 -0600

    adding main class for paper results

--- retire/__init__.py
+++ retire/__init__.py
@@ -1,3 +1,5 @@
 # retire/__init__.py
 
 from retire.explore import Explore
+
+from retire.retire import Retire
--- retire/retire.py
+++ retire/retire.py
@@ -1,9 +1,32 @@
 # retire/retire.py
 
-from retire.data import load_dataset
+
+import pandas as pd
+from importlib.resources import files
+from retire.data import load_dataset, load_graph
 
 
 class Retire:
 
     def __init__(self):
-        self.raw_df = load_dataset()
+        self.raw_df = load_dataset()    
+        self.graph = load_graph() 
+
+
+
+    def get_plant_level_analysis(self, ORISPL: str): 
+        pass 
+
+
+    def get_group_report(self ): 
+        path = files("retire").joinpath("resources/results/group_analysis.csv")
+        return pd.read_csv(path)
+
+    def get_target_explanations(self): 
+        path = files("retire").joinpath("resources/results/plant_level_match_explanations.csv")
+        return pd.read_csv(path)
+
+
+
+
+    
